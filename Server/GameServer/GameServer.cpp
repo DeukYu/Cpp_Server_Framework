2c64c092bc41a4044babaf571ba846171453d0fa
DeukYu <ldy8070@naver.com>
2022-01-04 01:33:17 +0900

    Create Thread
    쓰레드 생성

---
+++ Server/GameServer/GameServer.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <thread>
+
+void HelloThread()
+{
+	cout << "Hello Thread" << endl;
+}
+
+void HelloThread_2(int32 num)
+{
+	cout << num << endl;
+}
+
+int main()
+{
+	std::thread t;
+
+	vector<std::thread> v;
+
+	auto id1 = t.get_id();	// 쓰레드마다 id
+
+	for (int32 i = 0; i < 10; ++i)
+	{
+		v.push_back(std::thread(HelloThread_2, i));
+	}
+	
+	int32 count = t.hardware_concurrency();	// CPU 코어 개수?
+	auto id2 = t.get_id();	// 쓰레드마다 id
+	cout << "Hello Main" << endl;
+	for (int32 i = 0; i < 10; ++i)
+	{
+		if (v[i].joinable())
+			v[i].join();
+	}
+}
