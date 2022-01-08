aca892405de7ce7ae4f17fccb4ca8a53fe9942a1
DeukYu <ldy8070@naver.com>
2022-01-08 15:45:30 +0900

    캐시 실습
    캐시

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -3,83 +3,45 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <windows.h>
 #include <future>
 
-int64 result;
-
-int64 Calculate()
-{
-	int64 sum = 0;
-	for (int32 i = 0; i < 100'000; ++i)
-		sum += 1;
-	return sum;
-}
-
-void PromiseWorker(std::promise<string>&& promise)
-{
-	promise.set_value("Secret Message");
-}
-
-void TaskWorker(std::packaged_task<int64(void)>&& task)
-{
-	task();
-}
+int32 buffer[10'000][10'000];
 
 int main()
 {
-	// std::future
-	{
-		// 1) deferred : lazy evalution 지연해서 실행하세요.
-		// 2) async	: 별도의 쓰레드를 만들어서 실행하세요.
-		// 3) deferred | async	: 둘 중 알아서 골라주세요.
-		std::future<int64>	future = std::async(std::launch::async, Calculate);
+	memset(buffer, 0, sizeof(buffer));
 
-		/*std::future_status status = future.wait_for(1ms);
-		if (future_status::ready == status)
+	{
+		uint64 start = GetTickCount64();
+
+		int64	sum = 0;
+
+		for (int32 i = 0; i < 10'000; ++i)
 		{
+			for (int32 j = 0; j < 10'000; ++j)
+			{
+				sum += buffer[i][j];
+			}
+		}
 
-		}*/
+		uint64 end = GetTickCount64();
+		cout << "Elapsed Tick" << (end - start) << endl;
+	}
+	{
+		uint64 start = GetTickCount64();
 
+		int64	sum = 0;
 
-		int64 sum = future.get();	// 결과물이 이제서야 필요하다.
-
-		class Knight
+		for (int32 i = 0; i < 10'000; ++i)
 		{
-		public: 
-			int64 GetHp() { return 100; }
-		};
+			for (int32 j = 0; j < 10'000; ++j)
+			{
+				sum += buffer[j][i];
+			}
+		}
 
-		Knight knight;
-		std::future<int64> future2 = std::async(std::launch::async, &Knight::GetHp, knight);
+		uint64 end = GetTickCount64();
+		cout << "Elapsed Tick" << (end - start) << endl;
 	}
-
-	// std::promise
-	{
-		// 미래(std::future)에 결과물을 반환해줄거라 약속 ( std::primise ) 해줘 (계약서)
-		std::promise<string> promise;
-		std::future<string> future = promise.get_future();
-
-		thread t(PromiseWorker, std::move(promise));
-
-		string message = future.get();
-		cout << message << endl;
-
-		t.join();
-	}
-
-	// std::packaged_task
-	{
-		std::packaged_task<int64(void)> task(Calculate);
-		std::future<int64> future = task.get_future();
-
-		std::thread t(TaskWorker, std::move(task));
-
-		int64 sum = future.get();
-		cout << sum << endl;
-
-		t.join();
-	}
-
-	// 결론 )
-	// mutex, codition_Variable까지 가지고 않고 단순한 예들을 처리할 수 있는 
 }
