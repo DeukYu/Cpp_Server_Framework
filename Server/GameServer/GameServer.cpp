73e2088884b5d99c12f2afe06787bdf611dffefa
DeukYu <ldy8070@naver.com>
2022-01-08 15:25:57 +0900

    Future
    Future 함수 관련 기능들

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -3,57 +3,83 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
-#include <Windows.h>
+#include <future>
 
-mutex m;
-queue<int32> q;
-HANDLE handle;
+int64 result;
 
-void Producer()
+int64 Calculate()
 {
-	while (true)
-	{
-		{
-			unique_lock<mutex> lock(m);
-			q.push(100);
-		}
-		::SetEvent(handle);
-
-		this_thread::sleep_for(100ms);
-
-		
-	}
+	int64 sum = 0;
+	for (int32 i = 0; i < 100'000; ++i)
+		sum += 1;
+	return sum;
 }
 
-void Consumer()
+void PromiseWorker(std::promise<string>&& promise)
 {
-	while (true)
-	{
-		::WaitForSingleObject(handle, INFINITE);
+	promise.set_value("Secret Message");
+}
 
-		unique_lock<mutex>	lock(m);
-		if (q.empty() == false)
-		{
-			int32 data = q.front();
-			q.pop();
-			cout << data << endl;
-		}
-	}
+void TaskWorker(std::packaged_task<int64(void)>&& task)
+{
+	task();
 }
 
 int main()
 {
-	// 커널 오브젝트
-	// Usage Count
-	// Signal (파란불) / Non-Signal (빨간불) << bool
-	// Auto / Manual << bool
-	handle == ::CreateEvent(NULL/*보안속성*/, FALSE/*bManualReset*/, FALSE/*bInitialState*/, NULL);
+	// std::future
+	{
+		// 1) deferred : lazy evalution 지연해서 실행하세요.
+		// 2) async	: 별도의 쓰레드를 만들어서 실행하세요.
+		// 3) deferred | async	: 둘 중 알아서 골라주세요.
+		std::future<int64>	future = std::async(std::launch::async, Calculate);
 
-	thread t1(Producer);
-	thread t2(Consumer);
+		/*std::future_status status = future.wait_for(1ms);
+		if (future_status::ready == status)
+		{
 
-	t1.join();
-	t2.join();
+		}*/
 
-	::CloseHandle(handle);
+
+		int64 sum = future.get();	// 결과물이 이제서야 필요하다.
+
+		class Knight
+		{
+		public: 
+			int64 GetHp() { return 100; }
+		};
+
+		Knight knight;
+		std::future<int64> future2 = std::async(std::launch::async, &Knight::GetHp, knight);
+	}
+
+	// std::promise
+	{
+		// 미래(std::future)에 결과물을 반환해줄거라 약속 ( std::primise ) 해줘 (계약서)
+		std::promise<string> promise;
+		std::future<string> future = promise.get_future();
+
+		thread t(PromiseWorker, std::move(promise));
+
+		string message = future.get();
+		cout << message << endl;
+
+		t.join();
+	}
+
+	// std::packaged_task
+	{
+		std::packaged_task<int64(void)> task(Calculate);
+		std::future<int64> future = task.get_future();
+
+		std::thread t(TaskWorker, std::move(task));
+
+		int64 sum = future.get();
+		cout << sum << endl;
+
+		t.join();
+	}
+
+	// 결론 )
+	// mutex, codition_Variable까지 가지고 않고 단순한 예들을 처리할 수 있는 
 }
