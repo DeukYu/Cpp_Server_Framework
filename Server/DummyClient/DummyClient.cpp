289063ce8efc2440f03beedb70e92088e7d7ca5a
DeukYu <ldy8070@naver.com>
2022-02-11 00:19:37 +0900

    Socket Programming
    Socket Programming

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -1,6 +1,59 @@
 ﻿#include "pch.h"
 #include <iostream>
 
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
 int main()
 {
+	// 윈속 초기화 (ws2_32 라이브러리 초기화)
+	// 관련 정보가 wsaData에 채워짐
+	WSAData	 wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	// ad : Address Family ( AF_INET = IPv4, AF_INET6 = IPv6)
+	// type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
+	// protocol : 0
+	// return : descriptor
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// 연결할 목적지는? (IP주소 + Port)
+	SOCKADDR_IN	serverAddr;	//	IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	//serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1");
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	serverAddr.sin_port = ::htons(7777);	// host to network short
+
+	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// -------------------------
+	// 연결 성공! 데이터 송수신 가능!
+
+	cout << "Connected To Server!" << endl;
+
+	while (true)
+	{
+		// TODO
+		this_thread::sleep_for(1s);
+	}
+
+	// -------------------------
+	::closesocket(clientSocket);
+
+	::WSACleanup();
 }
