ee205fd095cd0e81225dd2ddc2d4dfb8c6f998e7
DeukYu <ldy8070@naver.com>
2022-02-16 23:42:38 +0900

    Socket Utils
    Socket Utils

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -1,52 +1,49 @@
 ﻿#include "pch.h"
 #include <iostream>
 
-#include <WinSock2.h>
-#include <MSWSock.h>
-#include <WS2tcpip.h>
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
 void HandleError(const char* cause)
 {
 	int32 errCode = ::WSAGetLastError();
-	cout << cause << "Socket ErrorCode : " << errCode << endl;
+	cout << cause << " ErrorCode : " << errCode << endl;
 }
 
 int main()
 {
-	WSAData	 wsaData;
+	WSAData wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
 
-	SOCKET	clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSocket == INVALID_SOCKET)
-	{
-		HandleError("Socket");
 		return 0;
-	}
 
-	u_long	on = 1;
+	u_long on = 1;
 	if (::ioctlsocket(clientSocket, FIONBIO, &on) == INVALID_SOCKET)
-	{
-		HandleError("IoctlSocket");
 		return 0;
-	}
 
-	SOCKADDR_IN	serverAddr;
+	SOCKADDR_IN serverAddr;
 	::memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
 	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
 	serverAddr.sin_port = ::htons(7777);
 
+	// Connect
 	while (true)
 	{
 		if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
 		{
+			// 원래 블록했어야 했는데... 너가 논블로킹으로 하라며?
 			if (::WSAGetLastError() == WSAEWOULDBLOCK)
 				continue;
-
+			// 이미 연결된 상태라면 break
 			if (::WSAGetLastError() == WSAEISCONN)
 				break;
+			// Error
 			break;
 		}
 	}
@@ -54,40 +51,42 @@ int main()
 	cout << "Connected to Server!" << endl;
 
 	char sendBuffer[100] = "Hello World";
+	WSAEVENT wsaEvent = ::WSACreateEvent();
+	WSAOVERLAPPED overlapped = {};
+	overlapped.hEvent = wsaEvent;
 
+	// Send
 	while (true)
 	{
-		if (::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0) == SOCKET_ERROR)
-		{
-			if (::WSAGetLastError() == WSAEWOULDBLOCK)
-				continue;
+		WSABUF wsaBuf;
+		wsaBuf.buf = sendBuffer;
+		wsaBuf.len = 100;
 
-			break;
+		DWORD sendLen = 0;
+		DWORD flags = 0;
+		if (::WSASend(clientSocket, &wsaBuf, 1, &sendLen, flags, &overlapped, nullptr) == SOCKET_ERROR)
+		{
+			if (::WSAGetLastError() == WSA_IO_PENDING)
+			{
+				// Pending
+				::WSAWaitForMultipleEvents(1, &wsaEvent, TRUE, WSA_INFINITE, FALSE);
+				::WSAGetOverlappedResult(clientSocket, &overlapped, &sendLen, FALSE, &flags);
+			}
+			else
+			{
+				// 진짜 문제 있는 상황
+				break;
+			}
 		}
 
 		cout << "Send Data ! Len = " << sizeof(sendBuffer) << endl;
 
-		char recvBuffer[1000];
-		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-		if (recvLen == SOCKET_ERROR)
-		{
-			if (::WSAGetLastError() == WSAEWOULDBLOCK)
-				continue;
-
-			break;
-		}
-		else if (recvLen == 0)
-		{
-			// 연결 끊김
-			break;
-		}
-
-		cout << "Recv Data Len = " << recvLen << endl;
-
 		this_thread::sleep_for(1s);
 	}
 
+	// 소켓 리소스 반환
 	::closesocket(clientSocket);
 
+	// 윈속 종료
 	::WSACleanup();
 }
--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -7,111 +7,24 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include <WinSock2.h>
-#include <MSWSock.h>
-#include <WS2tcpip.h>
-#pragma comment(lib, "ws2_32.lib")
-
-void HandleError(const char* cause)
-{
-	int32 errCode = ::WSAGetLastError();
-	cout << cause << "Socket ErrorCode : " << errCode << endl;
-}
+#include "SocketUtils.h"
 
 int main()
 {
-	WSAData	 wsaData;
-	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-		return 0;
+	SOCKET socket = SocketUtils::CreateSocket();
 
-	SOCKET	listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
-	if (listenSocket == INVALID_SOCKET)
-	{
-		HandleError("Socket");
-		return 0;
-	}
+	SocketUtils::BindAnyAddress(socket, 7777);
 
-	u_long	on = 1;
-	if (::ioctlsocket(listenSocket, FIONBIO, &on) == INVALID_SOCKET)
-	{
-		HandleError("IoctlSocket");
-		return 0;
-	}
+	SocketUtils::Listen(socket);
 
-	SOCKADDR_IN	serverAddr;
-	::memset(&serverAddr, 0, sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serverAddr.sin_port = ::htons(7777);
+	SOCKET clientSocket = ::accept(socket, nullptr, nullptr);
 
-	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-	{
-		HandleError("Bind");
-		return 0;
-	}
+	cout << "Client Connected!" << endl;
 
-	if (::listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
-	{
-		HandleError("Bind");
-		return 0;
-	}
-
-	cout << "Accept" << endl;
-
-	SOCKADDR_IN	clientAddr;
-	int32 addrLen = sizeof(clientAddr);
-
-	// Accept
 	while (true)
 	{
-		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
-		if (clientSocket == INVALID_SOCKET)
-		{
-			if (::WSAGetLastError() == WSAEWOULDBLOCK)
-				continue;
 
-			break;
-		}
-
-		cout << "Cient Connected!" << endl;
-
-		// Recv
-		while (true)
-		{
-			char recvBuffer[1000];
-			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-			if (recvLen == SOCKET_ERROR)
-			{
-				if (::WSAGetLastError() == WSAEWOULDBLOCK)
-					continue;
-
-				break;
-			}
-			else if (recvLen == 0)
-			{
-				// 연결 끊김
-				break;
-			}
-
-			cout << "Recv Data Len = " << recvLen << endl;
-
-			// Send
-			while (true)
-			{
-				if (::send(clientSocket, recvBuffer, recvLen, 0) == SOCKET_ERROR)
-				{
-					if (::WSAGetLastError() == WSAEWOULDBLOCK)
-						continue;
-
-					break;
-				}
-
-				cout << "Send Data ! Len = " << recvLen << endl;
-
-				break;
-			}
-		}
 	}
 
-	::WSACleanup();
-} 
+	GThreadManager->Join();
+}
