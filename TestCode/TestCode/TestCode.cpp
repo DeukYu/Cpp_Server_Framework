7c65983d061dfab385317a38b78954b054552c16
이득유 <ldy8070>
2023-05-09 06:32:08 +0000

    uuid opensouce test

--- TestCode/TestCode/TestCode.cpp
+++ TestCode/TestCode/TestCode.cpp
@@ -1,37 +1,16 @@
 ﻿#include <iostream>
-#include <thread>
-#include <format>
-#include "rapidjson/document.h"
-#include "rapidjson/stringbuffer.h"
-#include "rapidjson/stringbuffer.h"
-#include "rapidjson/writer.h"
-
-using namespace rapidjson;
+#include <unordered_map>
 
 using namespace std;
 
-int main()
-{ 
-    // 1. Parse a JSON string into DOM.
-    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
-    Document d;
-    d.Parse(json);
+int main() 
+{
+	unordered_map<int, int> testuMap;
 
-    Value test;
-    test["name"] = "deukyu";
-    test["age"] = "16";
-
-    // 2. Modify it by DOM.
-    Value& s = d["stars"];
-    s.SetInt(s.GetInt() + 1);
-
-    // 3. Stringify the DOM
-    StringBuffer buffer;
-    Writer<StringBuffer> writer(buffer);
-    d.Accept(writer);
-
-    // Output {"project":"rapidjson","stars":11}
-    std::cout << buffer.GetString() << std::endl;
-    return 0;
+	for (int i = 1; i < 100; ++i)
+	{
+		testuMap.insert({ i, i * 10 });
+	}
+	cout << testuMap.load_factor() << endl;
 	return 0;
 }
